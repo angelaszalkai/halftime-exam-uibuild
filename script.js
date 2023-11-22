6e65dbde6a0a7cbce7a675c6762e2002c9af6f5d
angelaszalkai <angela.szalkai@gmail.com>
2023-11-22 14:12:09 +0200

    some css added

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ rootElement.insertAdjacentHTML('afterbegin', `
 <form>
 
 <h1>Visszajelzés</h1>
-<input type='text' placeholder='Tárgy'>
-<textarea placeholder="Megjegyzés"></textarea>
+<input type='text' placeholder='Tárgy' class='targymezo'>
+<textarea placeholder="Megjegyzés" class='megjegyzes'></textarea>
 
-<div>
+<div class='checkbox'>
 <input type="checkbox">
 <p>Elolvastam és elfogadom az Adatkezelési Tájékoztatót.</p>
 </div>
 
-<div>
+<div class='checkbox'>
 <input type="checkbox">
 <p>Szeretnék hirlevelet kapni.</p>
 </div>
